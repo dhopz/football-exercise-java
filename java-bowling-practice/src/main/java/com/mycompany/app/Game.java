2f7d3cf9f1c0f4fc34af4f5744db70d4608cba6e
dhopz <david_houpapa@hotmail.com>
2022-07-01 11:27:06 +0100

    getting started

---
+++ java-bowling-practice/src/main/java/com/mycompany/app/Game.java
@@ -0,0 +1,18 @@
+package com.mycompany.app;
+
+public class Game
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public void roll(int pins) {
+            
+        }
+    
+    public int score() {
+        return 0;
+    }
+        
+}
