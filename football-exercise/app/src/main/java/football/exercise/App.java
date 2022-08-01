ac0138a1413c190cc6b4ff9d39270c3fb5098030
dhopz <david_houpapa@hotmail.com>
2022-08-01 18:07:26 +0100

    amending imports

--- football-exercise/app/src/main/java/football/exercise/App.java
+++ football-exercise/app/src/main/java/football/exercise/App.java
@@ -3,17 +3,10 @@
  */
 package football.exercise;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-// import java.io.FileReader;
-// import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
